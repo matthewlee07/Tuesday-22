2db23cf112bf83db585e4da76e18935e79f5db32
Matt Lee <matthewlee07@gmail.com>
2017-08-22 12:34:28 -0400

    completed filter

--- app.js
+++ app.js
@@ -142,26 +142,26 @@
 // })();
 
 
-function fizzBuzz(countTo) {
-  let result = [];
-  for (let i = 1; i <= countTo; i++) {
-    switch (true) {
-      case (i % 5 === 0 && i % 3 === 0):
-        result.push("fizzbuzz");
-        break;
-      case (i % 3 === 0):
-        result.push("fizz");
-        break;
-      case (i % 5 === 0):
-        result.push("buzz");
-        break;
-      default:
-        result.push(i);
-        break;
-    }
-  }
-  return result; 
-}
+// function fizzBuzz(countTo) {
+//   let result = [];
+//   for (let i = 1; i <= countTo; i++) {
+//     switch (true) {
+//       case (i % 5 === 0 && i % 3 === 0):
+//         result.push("fizzbuzz");
+//         break;
+//       case (i % 3 === 0):
+//         result.push("fizz");
+//         break;
+//       case (i % 5 === 0):
+//         result.push("buzz");
+//         break;
+//       default:
+//         result.push(i);
+//         break;
+//     }
+//   }
+//   return result; 
+// }
 
 
 
@@ -177,25 +177,50 @@ function fizzBuzz(countTo) {
 
 
 // tests
-(function testFizzBuzz() {
-  // we'll use the variables in our test cases
-  const countTo = 16;
-  const expected = [
-    1, 2, 'fizz', 4, 'buzz', 'fizz', 7, 8, 'fizz',
-    'buzz', 11, 'fizz', 13, 14, 'fizzbuzz', 16
-  ];
+// (function testFizzBuzz() {
+//   // we'll use the variables in our test cases
+//   const countTo = 16;
+//   const expected = [
+//     1, 2, 'fizz', 4, 'buzz', 'fizz', 7, 8, 'fizz',
+//     'buzz', 11, 'fizz', 13, 14, 'fizzbuzz', 16
+//   ];
 
-  const actual = fizzBuzz(countTo) || [];
+//   const actual = fizzBuzz(countTo) || [];
 
-  if (
-    expected.length === actual.length &&
-    expected.every(function (item, index) {
-      return actual[index] === item;
-    })) {
+//   if (
+//     expected.length === actual.length &&
+//     expected.every(function (item, index) {
+//       return actual[index] === item;
+//     })) {
 
-    console.log('SUCCESS: fizzBuzz is working');
-  }
-  else {
-    console.log('FAILURE: fizzBuzz is not working');
-  }
-})();
+//     console.log('SUCCESS: fizzBuzz is working');
+//   }
+//   else {
+//     console.log('FAILURE: fizzBuzz is not working');
+//   }
+// })();
+
+
+// Return only names that begin with 'R'
+const myNames = ['Rich', 'Joe', 'Bhaumik', 'Ray'];
+
+const filteredNames = filter(myNames, function (name) {
+  // This is a "predicate function" - it's a function that only returns a boolean
+  return name[0] === 'R';
+});
+
+console.log(filteredNames) // => ['Rich', 'Ray']
+
+// TASK -- DEFINE YOUR FILTER FUNCTION BELOW:
+function filter(arr, fn) {
+  const newArray = [];
+  arr.forEach(function (el) {
+    if (el[0] === "R") {
+      newArray.push(el);
+    }
+  });
+  return newArray;
+
+}
+
+filter(myNames);
