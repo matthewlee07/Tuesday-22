f59941cf9cf457a75d751aba3653146b715cb4fb
Matt Lee <matthewlee07@gmail.com>
2017-08-22 12:10:38 -0400

    completed average

--- app.js
+++ app.js
@@ -78,19 +78,65 @@
 //  })();
 
 
-function hello(){
-  console.log('Hello world'); 
+// function hello(){
+//   console.log('Hello world'); 
+// }
+
+// function goodbye(){
+//   console.log("Goodbye world"); 
+// }
+
+// function repeat(n, fn){
+//   for(let i = 0; i < n; i++){
+//     fn(); 
+//   }
+// }
+
+// repeat(5, hello); 
+// repeat(5, goodbye); 
+
+function average(numbers) {
+  // forEach method
+  let total=0; 
+  numbers.forEach(el => total+=el)
+  return total/numbers.length;
 }
 
-function goodbye(){
-  console.log("Goodbye world"); 
-}
+/* From here down, you are not expected to 
+   understand.... for now :)  
+   
+   
+   Nothing to see here!
+   
+*/
 
-function repeat(n, fn){
-  for(let i = 0; i < n; i++){
-    fn(); 
+
+// tests
+
+function testFunctionWorks(fn, input, expected) {
+  if (fn(input) === expected) {
+    console.log('SUCCESS: `' + fn.name + '` works on `[' + input + ']`');
+    return true;
+  }
+  else {
+    console.log(
+      'FAILURE: `' + fn.name + '([' + input + '])` should be ' + expected +
+      ' but was ' + fn(input)
+    );
+    return false;
   }
 }
 
-repeat(5, hello); 
-repeat(5, goodbye); 
+(function runTests() {
+  const numList1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  const correctAns1 = 5.5;
+  const numList2 = [0, -1, 1];
+  const correctAns2 = 0;
+  
+  const testResults = [
+    testFunctionWorks(average, numList1, correctAns1),
+    testFunctionWorks(average, numList2, correctAns2)
+  ];
+  const numPassing = testResults.filter(function(result){ return result; }).length;
+  console.log(numPassing + ' out of ' + testResults.length + ' tests passing.')
+})();
