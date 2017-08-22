b9f99a271db741a5f763004d4113355f7bc1f0e9
John Seabolt <johnseabolt@Johns-MacBook-Pro.local>
2017-08-22 09:23:37 -0700

    fizzbuzz added

--- app.js
+++ app.js
@@ -7,7 +7,7 @@
 //     }
 //     i++;
 //   }
-  
+
 //   // for(let i=0; i<numbers.length; i++){
 //   //   if (numbers[i]>currentMax){
 //   //     currentMax=numbers[i];
@@ -15,8 +15,8 @@
 //   // }
 //   return currentMax;
 //  }
- 
- 
+
+
 //  function min(numbers) {
 //   let currentMin = 0;
 //   let i=0;
@@ -28,20 +28,20 @@
 //   }
 //   return currentMin
 //  }
- 
- 
- 
+
+
+
 //  /* From here down, you are not expected to 
 //     understand.... for now :)  
-    
-    
+
+
 //     Nothing to see here!
-    
+
 //  */
- 
- 
+
+
 //  // tests
- 
+
 //  function testFunctionWorks(fn, input, expected) {
 //    if (fn(input) === expected) {
 //      console.log('SUCCESS: `' + fn.name + '` works on `[' + input + ']`');
@@ -55,8 +55,8 @@
 //      return false;
 //    }
 //  }
- 
- 
+
+
 //  (function runTests() {
 //    // we'll use the variables in our test cases
 //    const numList1 = [-5, 28, 98, -20013, 0.7878, 22, 115];
@@ -65,14 +65,14 @@
 //    const numList2 = [0, 1, 2, 3, 4];
 //    const realMin2 = numList2[0];
 //    const realMax2 = numList2[4];
-   
+
 //    const testResults = [
 //      testFunctionWorks(max, numList1, realMax1),
 //      testFunctionWorks(max, numList2, realMax2),
 //      testFunctionWorks(min, numList1, realMin1),
 //      testFunctionWorks(min, numList2, realMin2),
 //    ];
-   
+
 //    const numPassing = testResults.filter(function(result){ return result; }).length;
 //    console.log(numPassing + ' out of ' + testResults.length + ' tests passing.');
 //  })();
@@ -95,12 +95,76 @@
 // repeat(5, hello); 
 // repeat(5, goodbye); 
 
-function average(numbers) {
-  // forEach method
-  let total=0; 
-  numbers.forEach(el => total+=el)
-  return total/numbers.length;
+// function average(numbers) {
+//   // forEach method
+//   let total=0; 
+//   numbers.forEach(el => total+=el)
+//   return total/numbers.length;
+// }
+
+// /* From here down, you are not expected to 
+//    understand.... for now :)  
+
+
+//    Nothing to see here!
+
+// */
+
+
+// // tests
+
+// function testFunctionWorks(fn, input, expected) {
+//   if (fn(input) === expected) {
+//     console.log('SUCCESS: `' + fn.name + '` works on `[' + input + ']`');
+//     return true;
+//   }
+//   else {
+//     console.log(
+//       'FAILURE: `' + fn.name + '([' + input + '])` should be ' + expected +
+//       ' but was ' + fn(input)
+//     );
+//     return false;
+//   }
+// }
+
+// (function runTests() {
+//   const numList1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+//   const correctAns1 = 5.5;
+//   const numList2 = [0, -1, 1];
+//   const correctAns2 = 0;
+
+//   const testResults = [
+//     testFunctionWorks(average, numList1, correctAns1),
+//     testFunctionWorks(average, numList2, correctAns2)
+//   ];
+//   const numPassing = testResults.filter(function(result){ return result; }).length;
+//   console.log(numPassing + ' out of ' + testResults.length + ' tests passing.')
+// })();
+
+
+function fizzBuzz(countTo) {
+  let result = [];
+  for (let i = 1; i <= countTo; i++) {
+    switch (true) {
+      case (i % 5 === 0 && i % 3 === 0):
+        result.push("fizzbuzz");
+        break;
+      case (i % 3 === 0):
+        result.push("fizz");
+        break;
+      case (i % 5 === 0):
+        result.push("buzz");
+        break;
+      default:
+        result.push(i);
+        break;
+    }
+  }
+  return result; 
 }
+
+
+
 
 /* From here down, you are not expected to 
    understand.... for now :)  
@@ -111,32 +175,27 @@ function average(numbers) {
 */
 
 
-// tests
 
-function testFunctionWorks(fn, input, expected) {
-  if (fn(input) === expected) {
-    console.log('SUCCESS: `' + fn.name + '` works on `[' + input + ']`');
-    return true;
+// tests
+(function testFizzBuzz() {
+  // we'll use the variables in our test cases
+  const countTo = 16;
+  const expected = [
+    1, 2, 'fizz', 4, 'buzz', 'fizz', 7, 8, 'fizz',
+    'buzz', 11, 'fizz', 13, 14, 'fizzbuzz', 16
+  ];
+
+  const actual = fizzBuzz(countTo) || [];
+
+  if (
+    expected.length === actual.length &&
+    expected.every(function (item, index) {
+      return actual[index] === item;
+    })) {
+
+    console.log('SUCCESS: fizzBuzz is working');
   }
   else {
-    console.log(
-      'FAILURE: `' + fn.name + '([' + input + '])` should be ' + expected +
-      ' but was ' + fn(input)
-    );
-    return false;
+    console.log('FAILURE: fizzBuzz is not working');
   }
-}
-
-(function runTests() {
-  const numList1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-  const correctAns1 = 5.5;
-  const numList2 = [0, -1, 1];
-  const correctAns2 = 0;
-  
-  const testResults = [
-    testFunctionWorks(average, numList1, correctAns1),
-    testFunctionWorks(average, numList2, correctAns2)
-  ];
-  const numPassing = testResults.filter(function(result){ return result; }).length;
-  console.log(numPassing + ' out of ' + testResults.length + ' tests passing.')
 })();
